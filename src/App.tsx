65a952f8f4555017ce6a3f3c2066315f703ff3e8
Shahzaib Hassan <shahzaibdiraj480@gmail.com>
2026-08-21 16:30:44 +0500

    Added new features and updated code

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from "./Components/Header";
+import "./index.css"; 
+import logo from "./logo.svg";
+import reactLogo from "./react.svg";
+
+
+
+export function App() {
+
+  return (
+  <Header/>
+  );
+}
