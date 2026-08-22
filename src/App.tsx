9baceecb129425a9ef0cf5c30e029a249c41e495
Shahzaib Hassan <shahzaibdiraj480@gmail.com>
2026-08-22 11:21:57 +0500

    Project 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from "./Components/Header";
+import { ProjectTwo } from "./Components/ProjectTwo";
 import "./index.css"; 
 import logo from "./logo.svg";
 import reactLogo from "./react.svg";
@@ -8,6 +9,9 @@ import reactLogo from "./react.svg";
 export function App() {
 
   return (
-  <Header/>
+    <>
+  {/* <Header/> */}
+  <ProjectTwo/>
+  </>
   );
 }
